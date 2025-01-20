1df640cbdbae324855a1df96ec4e901aebbb7058
stracovsky <michal.stracovsky@devcity.tech>
2025-01-21 00:08:29 +0100

    category-selector fix

--- components/ui/category-selector.tsx
+++ components/ui/category-selector.tsx
@@ -36,7 +36,7 @@ function CategorySelectorComponent({ categories }: CategorySelectorProps) {
           variant="outline"
           role="combobox"
           aria-expanded={isOpen}
-          className="w-full max-w-full relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 hover:text-white font-bold py-2 px-4 rounded"
+          className="w-full max-w-full relative flex justify-center sm:justify-between sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 hover:text-white font-bold py-2 px-4 rounded"
         >
           {value
             ? categories.find((category) => category._id === value)?.title
